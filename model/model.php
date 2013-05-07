8bb83f487b9ff329e8a40d756233d09db6844162
Liming.Ma <limingv5@gmail.com>
2013-05-07 23:34:15 +0800

    db

--- model/model.php
+++ model/model.php
@@ -7,19 +7,23 @@ class Model
 	
 	function __construct($isdb=true) {
 		if ($isdb) {
-			$this->runtime = new PDO(
-				"mysql:host=".DB_HOST.";dbname=".DB_NAME,
-				DB_USER, DB_PASSWORD,
-				array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES '".DB_CHARSET."';")
-			);
-
-			$this->prefix = DB_PREFIX;
+			$this->db();
 		}
 		date_default_timezone_set("Asia/Shanghai");
 	}
 
 	function __get($name) {
 		return $this->prefix.$name;
+	}
+	
+	public function db() {
+		$this->runtime = new PDO(
+			"mysql:host=".DB_HOST.";dbname=".DB_NAME,
+			DB_USER, DB_PASSWORD,
+			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES '".DB_CHARSET."';")
+		);
+
+		$this->prefix = DB_PREFIX;
 	}
 }
 ?>
