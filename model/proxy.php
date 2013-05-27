90cd505cc9f59216ff09a0c12ee434884024b4d8
Liming.Ma <limingv5@gmail.com>
2013-05-27 17:19:27 +0800

    Create proxy.php

---
+++ model/proxy.php
@@ -0,0 +1,26 @@
+<?php
+class Proxy extends Model
+{
+  private $curl;
+	
+	function __construct() {
+		parent::__construct();
+		
+		$this->curl = curl_init();
+		curl_setopt($this->curl, CURLOPT_TIMEOUT, 5);
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->curl, CURLOPT_HEADER, false);
+	}
+	
+	public function restReq($url, $data=array(), $method="GET") {
+		curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, $method);
+		curl_setopt($this->curl, CURLOPT_URL, $url);
+		if ($data) {
+			curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($data));
+		}
+		$result = curl_exec($this->curl);
+    curl_close($this->curl);
+    return $result;
+	}
+}
+?>
